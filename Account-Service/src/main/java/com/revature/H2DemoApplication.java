933de103279803922ab1662d4cb5456ace8e05dd
Jason W. Clary <42851898+jwclary@users.noreply.github.com>
2020-06-18 17:04:18 -0400

    got rid of comments

--- Account-Service/src/main/java/com/revature/H2DemoApplication.java
+++ Account-Service/src/main/java/com/revature/H2DemoApplication.java
@@ -3,10 +3,7 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
-//@EnableSwagger2
 public class H2DemoApplication {
 
 	public static void main(String[] args) {
