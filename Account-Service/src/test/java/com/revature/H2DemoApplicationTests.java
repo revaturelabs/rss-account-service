564549cce1eda9aaeb94f1477cb2b494f01a51f7
nathan albin <nathan@radhet.com>
2020-06-18 20:40:18 -0400

    added working test using restassured + junit

--- Account-Service/src/test/java/com/revature/H2DemoApplicationTests.java
+++ Account-Service/src/test/java/com/revature/H2DemoApplicationTests.java
@@ -1,11 +1,23 @@
 package com.revature;
 
-import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.is;
 import org.springframework.boot.test.context.SpringBootTest;
+import static io.restassured.RestAssured.get;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
 class H2DemoApplicationTests {
+	
+	@Test
+	public void testUserControllerGetAllUsers() {
 
+		get("http://localhost:9000/user/all")
+				.then()
+				.assertThat()
+				.statusCode(200)
+				.body("size()", is(1));
+	}
 	@Test
 	void contextLoads() {
 	}
