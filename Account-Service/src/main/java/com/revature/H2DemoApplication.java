6fa9eb1ac7dcff048c0a450d7717ee7873554d7c
thecheatisgod@gmail.com <thecheatisgod@gmail.com>
2020-11-09 17:09:34 -0700

    initial setup for kubernetes

--- Account-Service/src/main/java/com/revature/H2DemoApplication.java
+++ Account-Service/src/main/java/com/revature/H2DemoApplication.java
@@ -2,8 +2,10 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
+@RibbonClient(name = "rss-account-service")
 public class H2DemoApplication {
 
 	public static void main(String[] args) {
