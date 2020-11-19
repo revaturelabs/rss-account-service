efd233d8408151389ee895b376d86b765b1992e0
thecheatisgod@gmail.com <thecheatisgod@gmail.com>
2020-11-18 20:30:05 -0700

    discovery client enable

--- Account-Service/src/main/java/com/revature/H2DemoApplication.java
+++ Account-Service/src/main/java/com/revature/H2DemoApplication.java
@@ -2,9 +2,11 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @RibbonClient(name = "rss-account-service")
 public class H2DemoApplication {
 
